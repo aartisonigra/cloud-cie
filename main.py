03a48585d4cba739db9cc0cfe08fdd16ea231203
Aarti Sonigra <aartisonigra@gmail.com>
2026-03-05 12:09:08 +0530

    Update main.py with new changes

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def greet(name):
     return f"Hello, {name}! Welcome to github_push_demo."
 
 if __name__ == "__main__":
-    user = input("Enter your name: ")
+    user = input("Enter your enrollmnet number: ")
     print(greet(user))
